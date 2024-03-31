1e29de537cc858ef8ceaf288ce7c618016ac54e8
sunghogo <leinad5592@gmail.com>
2024-03-30 20:10:42 -0400

    S2: Generics update

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Generics
+{
+    // Creating reusable classes like this has performance disadvantages due to boxing and casting
+    //public class List
+    //{
+    //    public void Add(int numebr)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //    public int this[int index]
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+    //}
+
+    //public class BookList
+    //{
+    //    public void Add(BookList book)
+    //    {
+    //        throw new NotImplementedException();
+
+    //    }
+
+    //    public BookList this[int index]
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+    //}
+
+    //public class ObjectList
+    //{
+    //    public void Add(object value)
+    //    {
+    //        throw new NotImplementedException();
+
+    //    }
+
+    //    public object this[int index]
+    //    {
+    //        get
+    //        {
+    //            throw new NotImplementedException();
+    //        }
+    //    }
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
