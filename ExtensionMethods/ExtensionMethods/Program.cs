e1596f1bc3898e7a0a2a9248b3448cf33f140c01
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-04-05 09:29:10 -0400

    S2: L10. Extension Methods

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿// String Extension methods located here
+using System;
+
+// Instance methods have priority over extension methods
+// If you have source code of original class, change it there. Otherwise derive a child class, and change it there. Finally using extension methods to create methods.
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Have to be in the SAME namespace, or call using namespace  to call extension methods
+// Can also put the extension namespace where the original class is located
+//namespace ExtensionMethods
+namespace System
+{
+    // String is a sealed class and cannot be inherited from
+    //public class RichString : String{}
+
+    // Naming convention: Prefix with the class you are extending, and postfix with Extensions
+    // use this keyword as first argument to specify the actual instance of the class you are trying to extend
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException(
+                    "numberOfWords should be greater than or equal to 0"
+                );
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+            {
+                return str;
+            }
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
